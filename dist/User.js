68fa2605bf8d4dcce5f3642731bb83304ea18cf8
ortalla <ortal.lagziel@nice.com>
2016-07-14 14:56:57 +0300

    test

--- dist/User.js
+++ dist/User.js
@@ -22,7 +22,7 @@ angular.module('UserModule', ['pascalprecht.translate'])
     //     .useMissingTranslationHandlerLog();
     }])
 
-    .factory('UserService', function(){
+    .factory('User', function(){
         'use strict';
         var user = {};
 
